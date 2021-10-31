69e3a8180d749c054e28a220f95dddf1652d9a13
Jason White <jmw5598@gmail.com>
2021-10-31 10:34:50 -0400

    update

--- src/app/modules/shared/framing/components/framing-navbar/framing-navbar.component.ts
+++ src/app/modules/shared/framing/components/framing-navbar/framing-navbar.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { fromEvent } from 'rxjs';
 import { NavigationLink } from '../../models/navigation-link.model';
 
@@ -12,14 +12,20 @@ export class FramingNavbarComponent implements OnInit {
   @Input()
   public links: NavigationLink[] = [];
 
+  constructor(
+    private _changeDetectorRef: ChangeDetectorRef
+  ) { }
+
   ngOnInit(): void {
     fromEvent(window, 'click')
       .subscribe(event => {
-        // handle outside click of dropdown
+        this.links.forEach(e => e.isExpanded = false);
+        this._changeDetectorRef.markForCheck();
       });
   }
 
-  public expandLink(link: NavigationLink): void {
+  public expandLink($event: any, link: NavigationLink): void {
+    $event.stopPropagation();
     if (link.isExpanded) {
       link.isExpanded = false;
       return;
